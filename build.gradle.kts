fc6c98bb325ccfb495924ed054ee8983be5cf336
astatio <29357305+astatio@users.noreply.github.com>
2023-11-22 12:24:29 +0000

    put templating files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,28 @@
 plugins {
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.serialization") version "1.8.21"
+    kotlin("multiplatform") version "2.0.0-Beta1"
+    embeddedKotlin("plugin.serialization")
     application
-    id("com.github.ben-manes.versions") version "0.46.0"
 }
+
+
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+application {
+	mainClass.set("MainKt")
+}
+
 repositories {
     mavenCentral()
-
-
+    maven("https://jitpack.io/")
+    maven("https://plugins.gradle.org/m2/")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
@@ -29,6 +36,9 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.4")
     implementation("com.jcabi:jcabi-xml:0.27.2")
 
+	//DE-COROUTINATOR FOR STACKTRACES
+	implementation("dev.reformator.stacktracedecoroutinator:stacktrace-decoroutinator-jvm:2.3.6")
+
     //TEMPLATE ENGINE (FOR TRANSLATING STRINGS)
     implementation("dev.icerock.moko:resources:0.22.0")
 }
@@ -38,7 +48,13 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(21)
+    androidTarget()
+    iosArm64()
+    iosSimulatorArm64()
+	// The iosMain source set is created automatically
+
+	applyDefaultHierarchyTemplate()
 }
 
 application {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,20 @@
 
 rootProject.name = "akotlinator"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":shared")
