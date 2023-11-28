ee31aa73d1284dc03f50e2ed3081f8ba257868d3
astatio <29357305+astatio@users.noreply.github.com>
2023-11-28 14:56:42 +0000

    Continued changes to KMP

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,47 +1,41 @@
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
+        alias(libs.plugins.kotlinMultiplatform)
+        alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+        iosX64()
+        iosArm64()
+        iosSimulatorArm64()
 
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
+        androidTarget { compilations.all { kotlinOptions { jvmTarget = "1.8" } } }
+        jvm()
+
+        sourceSets {
+                val commonMain by getting {
+                        dependencies {
+                                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.0")
+                                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+                                implementation("io.ktor:ktor-client-core:2.2.4")
+                                implementation("io.ktor:ktor-client-cio:2.2.4")
+                                implementation("io.ktor:ktor-client-content-negotiation:2.2.4")
+                                implementation("io.ktor:ktor-serialization-kotlinx-xml:2.2.4")
+                                implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.4")
+                                implementation("com.jcabi:jcabi-xml:0.27.2")
+                        }
+                }
+                val commonTest by getting {
+                        dependencies {
+                                implementation(kotlin("test-common"))
+                                implementation(kotlin("test-annotations-common"))
+                        }
+                }
         }
-    }
-
-    jvm()
-
-    sourceSets {
-        commonMain.dependencies {
-		testImplementation(kotlin("test"))
-		testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-		testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-
-	    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-
-		implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-    implementation("io.ktor:ktor-client-core:2.2.4")
-    implementation("io.ktor:ktor-client-cio:2.2.4")
-    implementation("io.ktor:ktor-client-content-negotiation:2.2.4")
-    implementation("io.ktor:ktor-serialization-kotlinx-xml:2.2.4")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.4")
-    implementation("com.jcabi:jcabi-xml:0.27.2")
-        }
-    }
 }
 
 android {
-    namespace = "org.example.project.shared"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
+        namespace = "org.example.project.shared"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        defaultConfig { minSdk = libs.versions.android.minSdk.get().toInt() }
 }
