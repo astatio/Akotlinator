daee5dcf8580e16594c277e1a8ce3da71989c33f
astatio <29357305+astatio@users.noreply.github.com>
2023-11-29 01:24:17 +0000

    Attemp at handling the publishment of packages

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,50 +1,88 @@
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
+import org.jetbrains.kotlin.gradle.utils.loadPropertyFromResources
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
-        alias(libs.plugins.kotlinMultiplatform)
-        alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    `maven-publish`
 }
+
+val localProperties = Properties().apply {
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        load(FileInputStream(localPropertiesFile))
+    }
+}
+
 
 kotlin {
-        listOf(
-                iosX64(),
-                iosArm64(),
-                iosSimulatorArm64()
-        ).forEach { iosTarget ->
-                iosTarget.binaries.framework {
-                        baseName = "Shared"
-                        isStatic = true
-                }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "Shared"
+            isStatic = true
         }
+    }
 
-        androidTarget {
-                compilations.all {
-                        kotlinOptions {
-                                jvmTarget = "1.8"
-                        }
-                }
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
         }
+    }
 
-        sourceSets {
-                commonMain.dependencies {
-                        implementation(libs.kotlinx.coroutines.core)
-                        implementation(libs.kotlinx.serialization.core)
-                        implementation(libs.kotlinx.serialization.json)
-                        implementation(libs.ktor.client.core)
-                        implementation(libs.ktor.client.cio)
-                        implementation(libs.ktor.client.content.negotiation)
-                        implementation(libs.ktor.serialization.kotlinx.xml)
-                        implementation(libs.ktor.serialization.kotlinx.json)
-                        implementation(libs.jcabi.xml)
-                }
-                commonTest.dependencies {
-                        implementation(kotlin("test"))
-                }
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.serialization.core)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.cio)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.xml)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.jcabi.xml)
         }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["kotlin"])
+                groupId = "com.github.astatio"
+                artifactId = "akotlinator" // Replace with your artifact ID
+                version = "0.0.2" // Replace with your version
+
+                artifact("$buildDir/libs/aar/my-library-release.aar")
+            }
+        }
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/astatio/Akotlinator")
+                credentials {
+                    username = localProperties["gpr.user"] as String?
+                    password = localProperties["gpr.key"] as String?
+                }
+            }
+        }
+    }
 }
 
+
+
 android {
-        namespace = "org.example.project.shared"
-        compileSdk = libs.versions.android.compileSdk.get().toInt()
-        defaultConfig {
-                minSdk = libs.versions.android.minSdk.get().toInt()
-        }
+    namespace = "org.example.project.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
 }
